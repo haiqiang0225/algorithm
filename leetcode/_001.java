7f0539b01740137a2959ed2b3f3515fe2874ea4b
haiqiang0225 <haiqiang0225@gmail.com>
2020-12-24 22:46:19 +0800

    add _001.java

---
+++ leetcode/_001.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _001 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int result = target - nums[i];
+            if (map.containsKey(result)) {
+                return new int[]{map.get(result), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(new _001().twoSum(nums, target)));
+    }
+
+}
+
